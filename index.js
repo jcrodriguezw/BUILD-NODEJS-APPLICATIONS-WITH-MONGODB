f693411d6653453f7a3bfa19530670a3477e968d
juan <jcrodriguezw@gmail.com>
2019-08-28 16:50:39 -0500

    Introduction to express framework

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express')
 
-const homePage = fs.readFileSync('index.html')
-const about = fs.readFileSync('about.html')
-const contact = fs.readFileSync('contact.html')
+const server = express()
 
-const server = http.createServer( (req, res) => {
-  // console.log(req.url)
-  if(req.url === '/about'){
-    return res.end(about)
-  } else if(req.url === '/contact'){
-    return res.end(contact)
-  } else if(req.url === '/'){
-    return res.end(homePage)
-  } else {
-    res.writeHead(404)
-    res.end('the page was not found')
-  }
+server.get('/', (req, res) => {
+  res.json({
+    name:'Katy Franz'
+  })
 })
 
-server.listen(4000)
+server.get('/about', (req, res) => {
+  res.send({
+    name:'Katy Franz'
+  })
+})
+
+server.listen(4000, () => {
+  console.log('server on port 4000')
+})
+
+
+// const http = require('http')
+// const fs = require('fs')
+
+// const homePage = fs.readFileSync('index.html')
+// const about = fs.readFileSync('about.html')
+// const contact = fs.readFileSync('contact.html')
+
+// const server = http.createServer( (req, res) => {
+//   // console.log(req.url)
+//   if(req.url === '/about'){
+//     return res.end(about)
+//   } else if(req.url === '/contact'){
+//     return res.end(contact)
+//   } else if(req.url === '/'){
+//     return res.end(homePage)
+//   } else {
+//     res.writeHead(404)
+//     res.end('the page was not found')
+//   }
+// })
+
+// server.listen(4000)
