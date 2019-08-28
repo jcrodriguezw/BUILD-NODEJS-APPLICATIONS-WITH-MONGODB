5ad38e31a23c8f91f14de76c819757950b970cdc
juan <jcrodriguezw@gmail.com>
2019-08-28 16:20:01 -0500

    Responding with HTML

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const http = require('http')
+const fs = require('fs')
+
+const homePage = fs.readFileSync('index.html')
+const about = fs.readFileSync('about.html')
+const contact = fs.readFileSync('contact.html')
 
 const server = http.createServer( (req, res) => {
   // console.log(req.url)
   if(req.url === '/about'){
-    return res.end('about page')
+    return res.end(about)
   } else if(req.url === '/contact'){
-    return res.end('contac page')
+    return res.end(contact)
   } else if(req.url === '/'){
-    return res.end('home page')
+    return res.end(homePage)
   } else {
     res.writeHead(404)
     res.end('the page was not found')
