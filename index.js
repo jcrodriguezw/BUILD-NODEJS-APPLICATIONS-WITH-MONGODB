e3d202db9773c085e578153821ca5ca481f5b4ab
juan <jcrodriguezw@gmail.com>
2019-08-29 09:14:15 -0500

    Creating the home page

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'pages/index.html'))
+})
 
 app.listen(5000, () => {
   console.log('server on port 5000')
