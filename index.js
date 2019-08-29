3dc62d6109f99e96a6bbcf789b2bac7ea50fd388
juan <jcrodriguezw@gmail.com>
2019-08-29 10:10:18 -0500

    Creating the about page

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname,'pages/index.html'))
 })
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'pages/about.html'))
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+})
+
 app.listen(5000, () => {
   console.log('server on port 5000')
 })
