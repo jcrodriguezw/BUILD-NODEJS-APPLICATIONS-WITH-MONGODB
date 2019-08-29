799779a28a2570a38ad2ce3d0babcd518302d11d
juan <jcrodriguezw@gmail.com>
2019-08-29 11:22:39 -0500

    Reading data from database using mongoose

--- test.js
+++ test.js
@@ -3,10 +3,20 @@ const Post = require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost/node-js-test-blog')
 
-Post.create({
-  title:'My first blog post',
-  description: 'Blog',
-  content: 'the content'
-}, (error, post) =>{
-  console.log(error, post)
+Post.find({
+  title:'My second blog post'
+}, (error,posts) => {
+  console.log(error, posts)
 })
+
+// Post.find({}, (error,posts) => {
+//   console.log(error, posts)
+// })
+
+// Post.create({
+//   title:'My second blog post',
+//   description: 'Blog',
+//   content: 'the content'
+// }, (error, post) =>{
+//   console.log(error, post)
+// })
